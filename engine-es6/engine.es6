8c5353db44f8a63b2ba40899336caa854720d6ba
Nikolai Svakhin <svakhine@adobe.com>
2016-05-09 21:59:14 -0700

    Bunch of tweeners added

--- engine-es6/engine.es6
+++ engine-es6/engine.es6
@@ -169,45 +169,248 @@ function init() {
     const loaderPromise = OBJLoadPromise('assets/models/obj/numbers_ring/numbers_ring.obj', manager,
         onProgress);
 
-    /**
-     * OBJLoadPromise(): Returns a promist to load OBJ
-     * @param {Number} min relative to web root
-     * @param {Number} max, instance of THREE.LoadingManager()
-     * @param {Number} value, progress callback()
-     * @return {Number} smooth value (0-1)
-     */
-    function smoothstep(min, max, value) {
-        var x = Math.max(0, Math.min(1, (value - min) / (max - min)));
-        return x * x * (3 - 2 * x);
-    }
+    /*
+    		t - current time of tween
+    		b - starting value of property
+    		c - change needed in value
+    		d - total duration of tween
+    	*/
+
+    // Ref: http://tweener.ivank.net/tw.js
+    var TweenFunc = {
+        /*
+        	t - current time of tween
+        	b - starting value of property
+        	c - change needed in value
+        	d - total duration of tween
+        */
+        easeNone: function(t, b, c, d) {
+            return c * t / d + b;
+        },
+        easeInQuad: function(t, b, c, d) {
+            return c * (t /= d) * t + b;
+        },
+        easeOutQuad: function(t, b, c, d) {
+            return -c * (t /= d) * (t - 2) + b;
+        },
+        easeInOutQuad: function(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t + b;
+            }
+            return -c / 2 * ((--t) * (t - 2) - 1) + b;
+        },
+        easeInCubic: function(t, b, c, d) {
+            return c * (t /= d) * t * t + b;
+        },
+        easeOutCubic: function(t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t + 1) + b;
+        },
+        easeInOutCubic: function(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t * t + b;
+            }
+            return c / 2 * ((t -= 2) * t * t + 2) + b;
+        },
+        easeOutInCubic: function(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutCubic(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInCubic((t * 2) - d, b + c / 2, c / 2, d);
+        },
+        easeInQuart: function(t, b, c, d) {
+            return c * (t /= d) * t * t * t + b;
+        },
+        easeOutQuart: function(t, b, c, d) {
+            return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+        },
+        easeInOutQuart: function(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t * t * t + b;
+            }
+            return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+        },
+        easeOutInQuart: function(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutQuart(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInQuart((t * 2) - d, b + c / 2, c / 2, d);
+        },
+        easeInQuint: function(t, b, c, d) {
+            return c * (t /= d) * t * t * t * t + b;
+        },
+        easeOutQuint: function(t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+        },
+        easeInOutQuint: function(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t * t * t * t + b;
+            }
+            return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
+        },
+        easeOutInQuint: function(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutQuint(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInQuint((t * 2) - d, b + c / 2, c / 2, d);
+        },
+        easeInSine: function(t, b, c, d) {
+            return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
+        },
+        easeOutSine: function(t, b, c, d) {
+            return c * Math.sin(t / d * (Math.PI / 2)) + b;
+        },
+        easeInOutSine: function(t, b, c, d) {
+            return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+        },
+        easeOutInSine: function(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutSine(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInSine((t * 2) - d, b + c / 2, c / 2, d);
+        },
+        easeInExpo: function(t, b, c, d) {
+            return (t === 0) ? b : c * Math.pow(2, 10 * (t / d - 1)) + b - c * 0.001;
+        },
+        easeOutExpo: function(t, b, c, d) {
+            return (t === d) ? b + c : c * 1.001 * (-Math.pow(2, -10 * t / d) + 1) + b;
+        },
+        easeInOutExpo: function(t, b, c, d) {
+            if (t === 0) {
+                return b;
+            }
+            if (t === d) {
+                return b + c;
+            }
+            if ((t /= d / 2) < 1) {
+                return c / 2 * Math.pow(2, 10 * (t - 1)) + b - c * 0.0005;
+            }
+            return c / 2 * 1.0005 * (-Math.pow(2, -10 * (t - 1)) + 2) + b;
+        },
+        easeOutInExpo: function(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeOutExpo(t * 2, b, c / 2, d);
+            return TweenFunc.easeInExpo((t * 2) - d, b + c / 2, c / 2, d);
+        },
+        easeInCirc: function(t, b, c, d) {
+            return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+        },
+        easeOutCirc: function(t, b, c, d) {
+            return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+        },
+        easeInOutCirc: function(t, b, c, d) {
+            if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
+            return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+        },
+        easeOutInCirc: function(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeOutCirc(t * 2, b, c / 2, d);
+            return TweenFunc.easeInCirc((t * 2) - d, b + c / 2, c / 2, d);
+        },
+        easeInElastic: function(t, b, c, d, a, p) {
+            var s;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (!p) p = d * .3;
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else s = p / (2 * Math.PI) * Math.asin(c / a);
+            return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+        },
+        easeOutElastic: function(t, b, c, d, a, p) {
+            var s;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (!p) p = d * .3;
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else s = p / (2 * Math.PI) * Math.asin(c / a);
+            return (a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b);
+        },
+        easeInOutElastic: function(t, b, c, d, a, p) {
+            var s;
+            if (t == 0) return b;
+            if ((t /= d / 2) == 2) return b + c;
+            if (!p) p = d * (.3 * 1.5);
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else s = p / (2 * Math.PI) * Math.asin(c / a);
+            if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 *
+                Math.PI) / p)) + b;
+            return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 +
+                c + b;
+        },
+        easeOutInElastic: function(t, b, c, d, a, p) {
+            if (t < d / 2) return TweenFunc.easeOutElastic(t * 2, b, c / 2, d, a, p);
+            return TweenFunc.easeInElastic((t * 2) - d, b + c / 2, c / 2, d, a, p);
+        },
+        easeInBack: function(t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            return c * (t /= d) * t * ((s + 1) * t - s) + b;
+        },
+        easeOutBack: function(t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+        },
+        easeInOutBack: function(t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= (1.525)) + 1) * t - s)) + b;
+            return c / 2 * ((t -= 2) * t * (((s *= (1.525)) + 1) * t + s) + 2) + b;
+        },
+        easeOutInBack: function(t, b, c, d, s) {
+            if (t < d / 2) return TweenFunc.easeOutBack(t * 2, b, c / 2, d, s);
+            return TweenFunc.easeInBack((t * 2) - d, b + c / 2, c / 2, d, s);
+        },
+        easeInBounce: function(t, b, c, d) {
+            return c - TweenFunc.easeOutBounce(d - t, 0, c, d) + b;
+        },
+        easeOutBounce: function(t, b, c, d) {
+            if ((t /= d) < (1 / 2.75)) {
+                return c * (7.5625 * t * t) + b;
+            } else if (t < (2 / 2.75)) {
+                return c * (7.5625 * (t -= (1.5 / 2.75)) * t + .75) + b;
+            } else if (t < (2.5 / 2.75)) {
+                return c * (7.5625 * (t -= (2.25 / 2.75)) * t + .9375) + b;
+            } else {
+                return c * (7.5625 * (t -= (2.625 / 2.75)) * t + .984375) + b;
+            }
+        },
+        easeInOutBounce: function(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeInBounce(t * 2, 0, c, d) * .5 + b;
+            else return TweenFunc.easeOutBounce(t * 2 - d, 0, c, d) * .5 + c * .5 + b;
+        },
+        easeOutInBounce: function(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeOutBounce(t * 2, b, c / 2, d);
+            return TweenFunc.easeInBounce((t * 2) - d, b + c / 2, c / 2, d);
+        }
+    };
 
     class AngleInterpolation {
-        constructor(iStartTime, iEndTime, iStartAngle, iEndAngle, iLerpType) {
+        constructor(iStartTime, iEndTime, iStartAngle, iEndAngle, iTweenFunc) {
             this.fStartTime = Number(iStartTime);
             this.fEndTime = Number(iEndTime);
             this.fStartAngle = iStartAngle;
             this.fEndAngle = iEndAngle;
-            this.fLerpType = iLerpType || "linear";
-        }
 
-        TransferNormalizedLerp(iT) {
-            if (this.fLerpType === "linear") {
-                return iT;
-            } else if (this.fLerpType === "smoothstep") {
-                return smoothstep(0.0, 1.0, iT);
-            }
-            return iT;
+
+            this.fTweenFunc = iTweenFunc;
         }
 
         LerpAngleConst(iCurrentTime) {
             const normalizedLerpTime = (iCurrentTime - this.fStartTime) /
                 (this.fEndTime - this.fStartTime);
 
+            const t = iCurrentTime - this.fStartTime;
+            const b = this.fStartAngle;
+            const c = this.fEndAngle - this.fStartAngle;
+            const d = this.fEndTime - this.fStartTime;
+
             let lerpedAngle = this.fStartAngle;
             if (normalizedLerpTime >= 0.0 && normalizedLerpTime < 1.0) {
-                const transferdLerp = this.TransferNormalizedLerp(normalizedLerpTime);
-                lerpedAngle = (1.0 - transferdLerp) * this.fStartAngle + (transferdLerp) * this
-                    .fEndAngle;
+                const tweenFunc = TweenFunc[this.fTweenFunc];
+
+                lerpedAngle = tweenFunc(t, b, c, d);
+
             } else if (normalizedLerpTime >= 1.0) {
                 lerpedAngle = this.fEndAngle;
             }
@@ -225,7 +428,7 @@ function init() {
     }
 
     class DialRing {
-        constructor(iMesh, iIndex, iLerpType) {
+        constructor(iMesh, iIndex, iTweenFunc) {
             this.fMesh = iMesh;
             this.fAngleRadians = 0.0;
             this.fIndex = iIndex;
@@ -243,7 +446,13 @@ function init() {
             this.fMesh.matrixAutoUpdate = false;
             this.fMesh.matrix.identity();
             this.fAngleInterpolation = null;
-            this.fLerpType = iLerpType;
+
+            if (TweenFunc.hasOwnProperty(iTweenFunc)) {
+                console.log("Function " + iTweenFunc + " accepted");
+            } else {
+                console.log("ERROR : No Tween Function " + iTweenFunc + " !");
+            }
+            this.fTweenFunc = iTweenFunc;
         }
 
         UpdatePosition() {
@@ -278,7 +487,7 @@ function init() {
 
         ScheduleAngleInterpolation(iNewAngleValue) {
             const lerpStruct = new AngleInterpolation(Date.now(), Date.now() + 700, this.fAngleRadians,
-                iNewAngleValue, this.fLerpType);
+                iNewAngleValue, this.fTweenFunc);
             this.fAngleInterpolation = lerpStruct;
         }
 
@@ -326,7 +535,7 @@ function init() {
 
                     gDials.push(dial);
 
-                    const dialRing = new DialRing(dial, i, "linear");
+                    const dialRing = new DialRing(dial, i, "easeNone");
                     gDialRings.push(dialRing);
                 }
 
@@ -433,7 +642,7 @@ function animate() {
         }
 
         if (nowMS - gLastSwitchArray[1] > 1000) {
-            gDialRings[1].fLerpType = "smoothstep";
+            gDialRings[1].fTweenFunc = "easeInOutQuad";
             gDialRings[1].ScheduleDialAdvance();
             gLastSwitchArray[1] = nowMS;
         }
--- engine/engine.js
+++ engine/engine.js
@@ -170,48 +170,246 @@ function init() {
     var onError = function onError() /* xhr */{};
     var loaderPromise = OBJLoadPromise('assets/models/obj/numbers_ring/numbers_ring.obj', manager, onProgress);
 
-    /**
-     * OBJLoadPromise(): Returns a promist to load OBJ
-     * @param {Number} min relative to web root
-     * @param {Number} max, instance of THREE.LoadingManager()
-     * @param {Number} value, progress callback()
-     * @return {Number} smooth value (0-1)
-     */
-    function smoothstep(min, max, value) {
-        var x = Math.max(0, Math.min(1, (value - min) / (max - min)));
-        return x * x * (3 - 2 * x);
-    }
+    /*
+    		t - current time of tween
+    		b - starting value of property
+    		c - change needed in value
+    		d - total duration of tween
+    	*/
+
+    // Ref: http://tweener.ivank.net/tw.js
+    var TweenFunc = {
+        /*
+        	t - current time of tween
+        	b - starting value of property
+        	c - change needed in value
+        	d - total duration of tween
+        */
+        easeNone: function easeNone(t, b, c, d) {
+            return c * t / d + b;
+        },
+        easeInQuad: function easeInQuad(t, b, c, d) {
+            return c * (t /= d) * t + b;
+        },
+        easeOutQuad: function easeOutQuad(t, b, c, d) {
+            return -c * (t /= d) * (t - 2) + b;
+        },
+        easeInOutQuad: function easeInOutQuad(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t + b;
+            }
+            return -c / 2 * (--t * (t - 2) - 1) + b;
+        },
+        easeInCubic: function easeInCubic(t, b, c, d) {
+            return c * (t /= d) * t * t + b;
+        },
+        easeOutCubic: function easeOutCubic(t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t + 1) + b;
+        },
+        easeInOutCubic: function easeInOutCubic(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t * t + b;
+            }
+            return c / 2 * ((t -= 2) * t * t + 2) + b;
+        },
+        easeOutInCubic: function easeOutInCubic(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutCubic(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInCubic(t * 2 - d, b + c / 2, c / 2, d);
+        },
+        easeInQuart: function easeInQuart(t, b, c, d) {
+            return c * (t /= d) * t * t * t + b;
+        },
+        easeOutQuart: function easeOutQuart(t, b, c, d) {
+            return -c * ((t = t / d - 1) * t * t * t - 1) + b;
+        },
+        easeInOutQuart: function easeInOutQuart(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t * t * t + b;
+            }
+            return -c / 2 * ((t -= 2) * t * t * t - 2) + b;
+        },
+        easeOutInQuart: function easeOutInQuart(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutQuart(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInQuart(t * 2 - d, b + c / 2, c / 2, d);
+        },
+        easeInQuint: function easeInQuint(t, b, c, d) {
+            return c * (t /= d) * t * t * t * t + b;
+        },
+        easeOutQuint: function easeOutQuint(t, b, c, d) {
+            return c * ((t = t / d - 1) * t * t * t * t + 1) + b;
+        },
+        easeInOutQuint: function easeInOutQuint(t, b, c, d) {
+            if ((t /= d / 2) < 1) {
+                return c / 2 * t * t * t * t * t + b;
+            }
+            return c / 2 * ((t -= 2) * t * t * t * t + 2) + b;
+        },
+        easeOutInQuint: function easeOutInQuint(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutQuint(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInQuint(t * 2 - d, b + c / 2, c / 2, d);
+        },
+        easeInSine: function easeInSine(t, b, c, d) {
+            return -c * Math.cos(t / d * (Math.PI / 2)) + c + b;
+        },
+        easeOutSine: function easeOutSine(t, b, c, d) {
+            return c * Math.sin(t / d * (Math.PI / 2)) + b;
+        },
+        easeInOutSine: function easeInOutSine(t, b, c, d) {
+            return -c / 2 * (Math.cos(Math.PI * t / d) - 1) + b;
+        },
+        easeOutInSine: function easeOutInSine(t, b, c, d) {
+            if (t < d / 2) {
+                return TweenFunc.easeOutSine(t * 2, b, c / 2, d);
+            }
+            return TweenFunc.easeInSine(t * 2 - d, b + c / 2, c / 2, d);
+        },
+        easeInExpo: function easeInExpo(t, b, c, d) {
+            return t === 0 ? b : c * Math.pow(2, 10 * (t / d - 1)) + b - c * 0.001;
+        },
+        easeOutExpo: function easeOutExpo(t, b, c, d) {
+            return t === d ? b + c : c * 1.001 * (-Math.pow(2, -10 * t / d) + 1) + b;
+        },
+        easeInOutExpo: function easeInOutExpo(t, b, c, d) {
+            if (t === 0) {
+                return b;
+            }
+            if (t === d) {
+                return b + c;
+            }
+            if ((t /= d / 2) < 1) {
+                return c / 2 * Math.pow(2, 10 * (t - 1)) + b - c * 0.0005;
+            }
+            return c / 2 * 1.0005 * (-Math.pow(2, -10 * (t - 1)) + 2) + b;
+        },
+        easeOutInExpo: function easeOutInExpo(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeOutExpo(t * 2, b, c / 2, d);
+            return TweenFunc.easeInExpo(t * 2 - d, b + c / 2, c / 2, d);
+        },
+        easeInCirc: function easeInCirc(t, b, c, d) {
+            return -c * (Math.sqrt(1 - (t /= d) * t) - 1) + b;
+        },
+        easeOutCirc: function easeOutCirc(t, b, c, d) {
+            return c * Math.sqrt(1 - (t = t / d - 1) * t) + b;
+        },
+        easeInOutCirc: function easeInOutCirc(t, b, c, d) {
+            if ((t /= d / 2) < 1) return -c / 2 * (Math.sqrt(1 - t * t) - 1) + b;
+            return c / 2 * (Math.sqrt(1 - (t -= 2) * t) + 1) + b;
+        },
+        easeOutInCirc: function easeOutInCirc(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeOutCirc(t * 2, b, c / 2, d);
+            return TweenFunc.easeInCirc(t * 2 - d, b + c / 2, c / 2, d);
+        },
+        easeInElastic: function easeInElastic(t, b, c, d, a, p) {
+            var s;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (!p) p = d * .3;
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else s = p / (2 * Math.PI) * Math.asin(c / a);
+            return -(a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+        },
+        easeOutElastic: function easeOutElastic(t, b, c, d, a, p) {
+            var s;
+            if (t == 0) return b;
+            if ((t /= d) == 1) return b + c;
+            if (!p) p = d * .3;
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else s = p / (2 * Math.PI) * Math.asin(c / a);
+            return a * Math.pow(2, -10 * t) * Math.sin((t * d - s) * (2 * Math.PI) / p) + c + b;
+        },
+        easeInOutElastic: function easeInOutElastic(t, b, c, d, a, p) {
+            var s;
+            if (t == 0) return b;
+            if ((t /= d / 2) == 2) return b + c;
+            if (!p) p = d * (.3 * 1.5);
+            if (!a || a < Math.abs(c)) {
+                a = c;
+                s = p / 4;
+            } else s = p / (2 * Math.PI) * Math.asin(c / a);
+            if (t < 1) return -.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p)) + b;
+            return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t * d - s) * (2 * Math.PI) / p) * .5 + c + b;
+        },
+        easeOutInElastic: function easeOutInElastic(t, b, c, d, a, p) {
+            if (t < d / 2) return TweenFunc.easeOutElastic(t * 2, b, c / 2, d, a, p);
+            return TweenFunc.easeInElastic(t * 2 - d, b + c / 2, c / 2, d, a, p);
+        },
+        easeInBack: function easeInBack(t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            return c * (t /= d) * t * ((s + 1) * t - s) + b;
+        },
+        easeOutBack: function easeOutBack(t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            return c * ((t = t / d - 1) * t * ((s + 1) * t + s) + 1) + b;
+        },
+        easeInOutBack: function easeInOutBack(t, b, c, d, s) {
+            if (s == undefined) s = 1.70158;
+            if ((t /= d / 2) < 1) return c / 2 * (t * t * (((s *= 1.525) + 1) * t - s)) + b;
+            return c / 2 * ((t -= 2) * t * (((s *= 1.525) + 1) * t + s) + 2) + b;
+        },
+        easeOutInBack: function easeOutInBack(t, b, c, d, s) {
+            if (t < d / 2) return TweenFunc.easeOutBack(t * 2, b, c / 2, d, s);
+            return TweenFunc.easeInBack(t * 2 - d, b + c / 2, c / 2, d, s);
+        },
+        easeInBounce: function easeInBounce(t, b, c, d) {
+            return c - TweenFunc.easeOutBounce(d - t, 0, c, d) + b;
+        },
+        easeOutBounce: function easeOutBounce(t, b, c, d) {
+            if ((t /= d) < 1 / 2.75) {
+                return c * (7.5625 * t * t) + b;
+            } else if (t < 2 / 2.75) {
+                return c * (7.5625 * (t -= 1.5 / 2.75) * t + .75) + b;
+            } else if (t < 2.5 / 2.75) {
+                return c * (7.5625 * (t -= 2.25 / 2.75) * t + .9375) + b;
+            } else {
+                return c * (7.5625 * (t -= 2.625 / 2.75) * t + .984375) + b;
+            }
+        },
+        easeInOutBounce: function easeInOutBounce(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeInBounce(t * 2, 0, c, d) * .5 + b;else return TweenFunc.easeOutBounce(t * 2 - d, 0, c, d) * .5 + c * .5 + b;
+        },
+        easeOutInBounce: function easeOutInBounce(t, b, c, d) {
+            if (t < d / 2) return TweenFunc.easeOutBounce(t * 2, b, c / 2, d);
+            return TweenFunc.easeInBounce(t * 2 - d, b + c / 2, c / 2, d);
+        }
+    };
 
     var AngleInterpolation = function () {
-        function AngleInterpolation(iStartTime, iEndTime, iStartAngle, iEndAngle, iLerpType) {
+        function AngleInterpolation(iStartTime, iEndTime, iStartAngle, iEndAngle, iTweenFunc) {
             _classCallCheck(this, AngleInterpolation);
 
             this.fStartTime = Number(iStartTime);
             this.fEndTime = Number(iEndTime);
             this.fStartAngle = iStartAngle;
             this.fEndAngle = iEndAngle;
-            this.fLerpType = iLerpType || "linear";
+
+            this.fTweenFunc = iTweenFunc;
         }
 
         _createClass(AngleInterpolation, [{
-            key: 'TransferNormalizedLerp',
-            value: function TransferNormalizedLerp(iT) {
-                if (this.fLerpType === "linear") {
-                    return iT;
-                } else if (this.fLerpType === "smoothstep") {
-                    return smoothstep(0.0, 1.0, iT);
-                }
-                return iT;
-            }
-        }, {
             key: 'LerpAngleConst',
             value: function LerpAngleConst(iCurrentTime) {
                 var normalizedLerpTime = (iCurrentTime - this.fStartTime) / (this.fEndTime - this.fStartTime);
 
+                var t = iCurrentTime - this.fStartTime;
+                var b = this.fStartAngle;
+                var c = this.fEndAngle - this.fStartAngle;
+                var d = this.fEndTime - this.fStartTime;
+
                 var lerpedAngle = this.fStartAngle;
                 if (normalizedLerpTime >= 0.0 && normalizedLerpTime < 1.0) {
-                    var transferdLerp = this.TransferNormalizedLerp(normalizedLerpTime);
-                    lerpedAngle = (1.0 - transferdLerp) * this.fStartAngle + transferdLerp * this.fEndAngle;
+                    var tweenFunc = TweenFunc[this.fTweenFunc];
+
+                    lerpedAngle = tweenFunc(t, b, c, d);
                 } else if (normalizedLerpTime >= 1.0) {
                     lerpedAngle = this.fEndAngle;
                 }
@@ -233,7 +431,7 @@ function init() {
     }();
 
     var DialRing = function () {
-        function DialRing(iMesh, iIndex, iLerpType) {
+        function DialRing(iMesh, iIndex, iTweenFunc) {
             _classCallCheck(this, DialRing);
 
             this.fMesh = iMesh;
@@ -253,7 +451,13 @@ function init() {
             this.fMesh.matrixAutoUpdate = false;
             this.fMesh.matrix.identity();
             this.fAngleInterpolation = null;
-            this.fLerpType = iLerpType;
+
+            if (TweenFunc.hasOwnProperty(iTweenFunc)) {
+                console.log("Function " + iTweenFunc + " accepted");
+            } else {
+                console.log("ERROR : No Tween Function " + iTweenFunc + " !");
+            }
+            this.fTweenFunc = iTweenFunc;
         }
 
         _createClass(DialRing, [{
@@ -289,7 +493,7 @@ function init() {
         }, {
             key: 'ScheduleAngleInterpolation',
             value: function ScheduleAngleInterpolation(iNewAngleValue) {
-                var lerpStruct = new AngleInterpolation(Date.now(), Date.now() + 700, this.fAngleRadians, iNewAngleValue, this.fLerpType);
+                var lerpStruct = new AngleInterpolation(Date.now(), Date.now() + 700, this.fAngleRadians, iNewAngleValue, this.fTweenFunc);
                 this.fAngleInterpolation = lerpStruct;
             }
         }, {
@@ -341,7 +545,7 @@ function init() {
 
                     gDials.push(dial);
 
-                    var dialRing = new DialRing(dial, i, "linear");
+                    var dialRing = new DialRing(dial, i, "easeNone");
                     gDialRings.push(dialRing);
                 }
             }
@@ -436,7 +640,7 @@ function animate() {
         }
 
         if (nowMS - gLastSwitchArray[1] > 1000) {
-            gDialRings[1].fLerpType = "smoothstep";
+            gDialRings[1].fTweenFunc = "easeInOutQuad";
             gDialRings[1].ScheduleDialAdvance();
             gLastSwitchArray[1] = nowMS;
         }
