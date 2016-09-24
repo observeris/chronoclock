7d6e8abe8f338d847f689c7374f5bd070adcef3b
Nikolai Svakhin <svakhine@adobe.com>
2016-09-24 10:18:16 -0700

    FTP deployment gulp script

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,125 +1,45 @@
-const gulp = require('gulp');
-const util = require('gulp-util');
-// const  concat = require('gulp-concat');
-const browserify = require('gulp-browserify');
-const babel = require('gulp-babel');
-const source = require('vinyl-source-stream');
-const rename = require('gulp-rename');
-const print = require('gulp-print');
-const shell = require('gulp-shell');
+//From https://www.npmjs.com/package/vinyl-ftp
+//`npm install vinyl-ftp --save-dev` before using
+var gulp = require('gulp');
+var gutil = require('gulp-util');
+var ftp = require('vinyl-ftp');
 
-const fs = require('fs');
-const path = require('path');
-const merge = require('merge-stream');
+var fs = require('fs');
+var deploy_config = JSON.parse(fs.readFileSync('./.deploy_config.json'));
+// MUST have a following ./.deploy_config.json in your root directory:
+// This needs to come from your hosting provide: e.g.:
+// https://my.bluehost.com/web-hosting/cplogin?goto_uri=/frontend/bluehost/ftp/accounts.html
+/* {
+    "host": < FTPHOST > ,
+    "user": < FTPUSERNAME >
+    "password": < FTP PASSWORD >
+} */
 
-const env = process.env.NODE_ENV || 'development';
+gulp.task('ftp-deploy', function() {
 
-util.log("ENV === '" + env + "'");
+  var conn = ftp.create({
+    host: deploy_config.host,
+    user: deploy_config.user,
+    password: deploy_config.password,
+    parallel: 1,
+    log: gutil.log
+  });
 
-const kBabelSourcesRegexp = '/**/*.es6';
-const kBabelDestinationExtension = '.js';
+  var globs = [
+    'assets/**',
+    'engine/**',
+    'external/**',
+    'index.html'
+  ];
 
-/**
- * A wrapper that prevents an error from stopping a watch
- * @param {error} error to print out.
- * (per http://stackoverflow.com/questions/23971388/prevent-errors-from-breaking-crashing-gulp-watch)
- */
-function swallowError(error) {
+  // using base = '.' will transfer everything to /public_html correctly
+  // turn off buffering in gulp.src for best performance
 
-    util.log("BABEL ERROR:" + error.toString());
+  return gulp.src(globs, {
+      base: '.',
+      buffer: false
+    })
+    .pipe(conn.newer('/apps/countdown4d/')) // only upload newer files
+    .pipe(conn.dest('/apps/countdown4d/'));
 
-    this.emit('end');
-}
-
-/**
- * Gets all the folders in a current folder, synchronously
- * @param {String} dir : current folder
- * @return {Array} array of directory names
- */
-function getFolders(dir) {
-    return fs.readdirSync(dir)
-        .filter(function(file) {
-            return fs.statSync(path.join(dir, file)).isDirectory();
-        });
-}
-
-/**
- * Gets all the source folders in iBabelSourcePath, including './'
- * @param {String} iBabelSourcePath, root of a tree to scan
- * @return {Array} array of subfolders' basenames
- */
-function getBabelSourceFolders(iBabelSourcePath) {
-    const folders = getFolders(iBabelSourcePath);
-    folders.push("./");
-    return folders;
-}
-
-/**
- * Run the babel tasks on all *.es6 files in the directory,
- * replicating the folder structure in the destination directory
- * @param {String} iBabelSourcePath, root of a tree to scan
- * @param {String} iBabelDestinationPath, destination path
- * @return {Array} array of subfolders' basenames
- */
-function babelFolderTree(iBabelSourcePath, iBabelDestinationPath) {
-
-    const babelFileLogPrintFunction = function(filepath) {
-        return "Babel Transcoding ES6: " + filepath;
-    };
-
-    const babelDestination = function(filepath) {
-        return "Babel Result JS is at : " + filepath;
-    };
-
-    const tasks = getBabelSourceFolders(iBabelSourcePath).map(function(iFolder) {
-
-        // See .babelrc for babel() configuration, we use es2015 profile.
-
-        return gulp.src(path.join(iBabelSourcePath, iFolder, kBabelSourcesRegexp))
-            .pipe(print(babelFileLogPrintFunction))
-            .pipe(babel().on('error', swallowError))
-            .pipe(rename(function(path) {
-                // path.basename += "-babelprocessed";
-                path.extname = kBabelDestinationExtension;
-                return path;
-            }))
-            .pipe(gulp.dest(path.join(iBabelDestinationPath, iFolder)))
-            .pipe(print(babelDestination));
-    });
-
-    //return merge(tasks);
-    return tasks;
-
-}
-/**
- * Put the directory structure in an array, for a gulp watch task
- * @param {String} iBabelSourcePath, root of a tree to scan
- * @return {Array} array of subfolders' full path names + es6 regexp search, to feed into gulp.watch()
- */
-function babelSourceFolderArray(iBabelSourcePath) {
-    const foldersWatchArray = [];
-
-    getBabelSourceFolders(iBabelSourcePath).forEach(function(iFolder) {
-        foldersWatchArray.push(path.join(iBabelSourcePath, iFolder, kBabelSourcesRegexp));
-    });
-
-    return foldersWatchArray;
-}
-
-const kBabelSourcePath = "engine-es6";
-const kBabelDestinationPath = "engine-babel-result";
-
-gulp.task("babel", () => {
-    babelFolderTree(kBabelSourcePath, kBabelDestinationPath);
 });
-
-gulp.task("watch", () => {
-    // livereload.listen();
-    gulp.watch(babelSourceFolderArray(kBabelSourcePath), ['babel']);
-});
-
-// gulp.task("browserify", ["babel"], shell.task([
-//     'browserify engine-babel-result/*.js -o engine/main.js',
-// ]));
-
-gulp.task('default', ['browserify', 'watch']);
---
+++ old_gulpfile.babel.js
@@ -0,0 +1,125 @@
+const gulp = require('gulp');
+const util = require('gulp-util');
+// const  concat = require('gulp-concat');
+const browserify = require('gulp-browserify');
+const babel = require('gulp-babel');
+const source = require('vinyl-source-stream');
+const rename = require('gulp-rename');
+const print = require('gulp-print');
+const shell = require('gulp-shell');
+
+const fs = require('fs');
+const path = require('path');
+const merge = require('merge-stream');
+
+const env = process.env.NODE_ENV || 'development';
+
+util.log("ENV === '" + env + "'");
+
+const kBabelSourcesRegexp = '/**/*.es6';
+const kBabelDestinationExtension = '.js';
+
+/**
+ * A wrapper that prevents an error from stopping a watch
+ * @param {error} error to print out.
+ * (per http://stackoverflow.com/questions/23971388/prevent-errors-from-breaking-crashing-gulp-watch)
+ */
+function swallowError(error) {
+
+    util.log("BABEL ERROR:" + error.toString());
+
+    this.emit('end');
+}
+
+/**
+ * Gets all the folders in a current folder, synchronously
+ * @param {String} dir : current folder
+ * @return {Array} array of directory names
+ */
+function getFolders(dir) {
+    return fs.readdirSync(dir)
+        .filter(function(file) {
+            return fs.statSync(path.join(dir, file)).isDirectory();
+        });
+}
+
+/**
+ * Gets all the source folders in iBabelSourcePath, including './'
+ * @param {String} iBabelSourcePath, root of a tree to scan
+ * @return {Array} array of subfolders' basenames
+ */
+function getBabelSourceFolders(iBabelSourcePath) {
+    const folders = getFolders(iBabelSourcePath);
+    folders.push("./");
+    return folders;
+}
+
+/**
+ * Run the babel tasks on all *.es6 files in the directory,
+ * replicating the folder structure in the destination directory
+ * @param {String} iBabelSourcePath, root of a tree to scan
+ * @param {String} iBabelDestinationPath, destination path
+ * @return {Array} array of subfolders' basenames
+ */
+function babelFolderTree(iBabelSourcePath, iBabelDestinationPath) {
+
+    const babelFileLogPrintFunction = function(filepath) {
+        return "Babel Transcoding ES6: " + filepath;
+    };
+
+    const babelDestination = function(filepath) {
+        return "Babel Result JS is at : " + filepath;
+    };
+
+    const tasks = getBabelSourceFolders(iBabelSourcePath).map(function(iFolder) {
+
+        // See .babelrc for babel() configuration, we use es2015 profile.
+
+        return gulp.src(path.join(iBabelSourcePath, iFolder, kBabelSourcesRegexp))
+            .pipe(print(babelFileLogPrintFunction))
+            .pipe(babel().on('error', swallowError))
+            .pipe(rename(function(path) {
+                // path.basename += "-babelprocessed";
+                path.extname = kBabelDestinationExtension;
+                return path;
+            }))
+            .pipe(gulp.dest(path.join(iBabelDestinationPath, iFolder)))
+            .pipe(print(babelDestination));
+    });
+
+    //return merge(tasks);
+    return tasks;
+
+}
+/**
+ * Put the directory structure in an array, for a gulp watch task
+ * @param {String} iBabelSourcePath, root of a tree to scan
+ * @return {Array} array of subfolders' full path names + es6 regexp search, to feed into gulp.watch()
+ */
+function babelSourceFolderArray(iBabelSourcePath) {
+    const foldersWatchArray = [];
+
+    getBabelSourceFolders(iBabelSourcePath).forEach(function(iFolder) {
+        foldersWatchArray.push(path.join(iBabelSourcePath, iFolder, kBabelSourcesRegexp));
+    });
+
+    return foldersWatchArray;
+}
+
+const kBabelSourcePath = "engine-es6";
+const kBabelDestinationPath = "engine-babel-result";
+
+gulp.task("babel", () => {
+    babelFolderTree(kBabelSourcePath, kBabelDestinationPath);
+});
+
+gulp.task("watch", () => {
+    // livereload.listen();
+    gulp.watch(babelSourceFolderArray(kBabelSourcePath), ['babel']);
+});
+
+// gulp.task("browserify", ["babel"], shell.task([
+//     'browserify engine-babel-result/*.js -o engine/main.js',
+// ]));
+
+gulp.task('default', ['browserify', 'watch']);
