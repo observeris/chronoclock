20e419099a91c98645468739d851e3755c3547a6
Nikolai Svakhin <svakhine@adobe.com>
2016-09-24 09:08:44 -0700

    Fixing path to the HDR file

--- webpack.config.js
+++ webpack.config.js
@@ -7,45 +7,45 @@ var path = require('path');
 // > npm install --save-dev glslify-loader raw-loader
 // > npm install --save-dev babel-loader babel-core babel-preset-es2015
 module.exports = {
-    context: __dirname,
-    module: {
-        loaders: [{
-            test: /\.scss$/,
-            loaders: ["style", "css", "sass"],
-        }, {
-            test: /\.html$/,
-            loader: "file?name=[name].[ext]",
-        }, {
-            test: /\.(js|es6)$/,
-            exclude: [
-                path.resolve('./external/three/build/three.js'),
-            ],
-            loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
-        }, {
-            test: /\.(jpg|png|gif|hdr)$/,
-            loader: "file?name=/[hash].[ext]",
-        }, {
-            test: /\.(frag|vert|glsl)$/,
-            loaders: ["raw", "glslify"]
-        }]
-    },
-    entry: {
-        app: [
-            "./engine-es6/main.es6"
-        ],
-    },
-    output: {
-        path: __dirname + "/engine",
-        filename: "./main.js",
-        publicPath: "/engine"
-    },
-    resolve: {
-        root: [
-            path.resolve('./external/three/build'),
-            path.resolve('./external/three/modules'),
-            path.resolve('./external/vdc'),
-            path.resolve('./external/superagent')
-        ],
-        extensions: ["", ".webpack.js", ".web.js", ".js", ".es6"]
-    }
+  context: __dirname,
+  module: {
+    loaders: [{
+      test: /\.scss$/,
+      loaders: ["style", "css", "sass"],
+    }, {
+      test: /\.html$/,
+      loader: "file?name=[name].[ext]",
+    }, {
+      test: /\.(js|es6)$/,
+      exclude: [
+        path.resolve('./external/three/build/three.js'),
+      ],
+      loaders: ['babel-loader?presets[]=react,presets[]=es2015'],
+    }, {
+      test: /\.(jpg|png|gif|hdr)$/,
+      loader: "file?name=/[hash].[ext]",
+    }, {
+      test: /\.(frag|vert|glsl)$/,
+      loaders: ["raw", "glslify"]
+    }]
+  },
+  entry: {
+    app: [
+      "./engine-es6/main.es6"
+    ],
+  },
+  output: {
+    path: __dirname + "/engine",
+    filename: "./main.js",
+    publicPath: "engine"
+  },
+  resolve: {
+    root: [
+      path.resolve('./external/three/build'),
+      path.resolve('./external/three/modules'),
+      path.resolve('./external/vdc'),
+      path.resolve('./external/superagent')
+    ],
+    extensions: ["", ".webpack.js", ".web.js", ".js", ".es6"]
+  }
 };
